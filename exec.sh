78cd639fdedb098975f67be2097992affc470864
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2026-04-23 12:06:33 +0900

    update exec.sh

--- exec.sh
+++ exec.sh
@@ -7,46 +7,46 @@ mkdir -p result/{${FAKE},${DEFAULT}}
 
 # with light request
 # thread: 1
-# taskset -c 0-3 ./${DEFAULT} -t 1 -m 0 > result/${DEFAULT}/t1_m0.txt
-# taskset -c 0-3 ./${DEFAULT} -t 1 -m 25 > result/${DEFAULT}/t1_m25.txt
-# taskset -c 0-3 ./${DEFAULT} -t 1 -m 50 > result/${DEFAULT}/t1_m50.txt
-# taskset -c 0-3 ./${DEFAULT} -t 1 -m 100 > result/${DEFAULT}/t1_m100.txt
-# taskset -c 0-3 ./${DEFAULT} -t 1 -m 200 > result/${DEFAULT}/t1_m200.txt
-# taskset -c 0-3 ./${DEFAULT} -t 1 -m 500 > result/${DEFAULT}/t1_m500.txt
-# taskset -c 0-3 ./${DEFAULT} -t 1 -m 1000 > result/${DEFAULT}/t1_m1000.txt
+taskset -c 0-3 ./${DEFAULT} -t 1 -m 0 > result/${DEFAULT}/t1_m0.txt
+taskset -c 0-3 ./${DEFAULT} -t 1 -m 25 > result/${DEFAULT}/t1_m25.txt
+taskset -c 0-3 ./${DEFAULT} -t 1 -m 50 > result/${DEFAULT}/t1_m50.txt
+taskset -c 0-3 ./${DEFAULT} -t 1 -m 100 > result/${DEFAULT}/t1_m100.txt
+taskset -c 0-3 ./${DEFAULT} -t 1 -m 200 > result/${DEFAULT}/t1_m200.txt
+taskset -c 0-3 ./${DEFAULT} -t 1 -m 500 > result/${DEFAULT}/t1_m500.txt
+taskset -c 0-3 ./${DEFAULT} -t 1 -m 1000 > result/${DEFAULT}/t1_m1000.txt
 taskset -c 0-3 ./${DEFAULT} -t 1 -m 5000 > result/${DEFAULT}/t1_m5000.txt
 taskset -c 0-3 ./${DEFAULT} -t 1 -m 10000 > result/${DEFAULT}/t1_m10000.txt
 # thread: 8
-# taskset -c 0-3 ./${DEFAULT} -t 8 -m 0 > result/${DEFAULT}/t8_m0.txt
-# taskset -c 0-3 ./${DEFAULT} -t 8 -m 25 > result/${DEFAULT}/t8_m25.txt
-# taskset -c 0-3 ./${DEFAULT} -t 8 -m 50 > result/${DEFAULT}/t8_m50.txt
-# taskset -c 0-3 ./${DEFAULT} -t 8 -m 100 > result/${DEFAULT}/t8_m100.txt
-# taskset -c 0-3 ./${DEFAULT} -t 8 -m 200 > result/${DEFAULT}/t8_m200.txt
-# taskset -c 0-3 ./${DEFAULT} -t 8 -m 500 > result/${DEFAULT}/t8_m500.txt
-# taskset -c 0-3 ./${DEFAULT} -t 8 -m 1000 > result/${DEFAULT}/t8_m1000.txt
+taskset -c 0-3 ./${DEFAULT} -t 8 -m 0 > result/${DEFAULT}/t8_m0.txt
+taskset -c 0-3 ./${DEFAULT} -t 8 -m 25 > result/${DEFAULT}/t8_m25.txt
+taskset -c 0-3 ./${DEFAULT} -t 8 -m 50 > result/${DEFAULT}/t8_m50.txt
+taskset -c 0-3 ./${DEFAULT} -t 8 -m 100 > result/${DEFAULT}/t8_m100.txt
+taskset -c 0-3 ./${DEFAULT} -t 8 -m 200 > result/${DEFAULT}/t8_m200.txt
+taskset -c 0-3 ./${DEFAULT} -t 8 -m 500 > result/${DEFAULT}/t8_m500.txt
+taskset -c 0-3 ./${DEFAULT} -t 8 -m 1000 > result/${DEFAULT}/t8_m1000.txt
 taskset -c 0-3 ./${DEFAULT} -t 8 -m 5000 > result/${DEFAULT}/t8_m5000.txt
 taskset -c 0-3 ./${DEFAULT} -t 8 -m 10000 > result/${DEFAULT}/t8_m10000.txt
 
 # with heavy request
 # thread: 1
-# taskset -c 0-3 ./${FAKE} -t 1 -m 0 > result/${FAKE}/t1_m0.txt
-# taskset -c 0-3 ./${FAKE} -t 1 -m 25 > result/${FAKE}/t1_m25.txt
-# taskset -c 0-3 ./${FAKE} -t 1 -m 50 > result/${FAKE}/t1_m50.txt
-# taskset -c 0-3 ./${FAKE} -t 1 -m 100 > result/${FAKE}/t1_m100.txt
-# taskset -c 0-3 ./${FAKE} -t 1 -m 200 > result/${FAKE}/t1_m200.txt
-# taskset -c 0-3 ./${FAKE} -t 1 -m 500 > result/${FAKE}/t1_m500.txt
-# taskset -c 0-3 ./${FAKE} -t 1 -m 1000 > result/${FAKE}/t1_m1000.txt
+taskset -c 0-3 ./${FAKE} -t 1 -m 0 > result/${FAKE}/t1_m0.txt
+taskset -c 0-3 ./${FAKE} -t 1 -m 25 > result/${FAKE}/t1_m25.txt
+taskset -c 0-3 ./${FAKE} -t 1 -m 50 > result/${FAKE}/t1_m50.txt
+taskset -c 0-3 ./${FAKE} -t 1 -m 100 > result/${FAKE}/t1_m100.txt
+taskset -c 0-3 ./${FAKE} -t 1 -m 200 > result/${FAKE}/t1_m200.txt
+taskset -c 0-3 ./${FAKE} -t 1 -m 500 > result/${FAKE}/t1_m500.txt
+taskset -c 0-3 ./${FAKE} -t 1 -m 1000 > result/${FAKE}/t1_m1000.txt
 taskset -c 0-3 ./${FAKE} -t 1 -m 5000 > result/${FAKE}/t1_m5000.txt
 taskset -c 0-3 ./${FAKE} -t 1 -m 10000 > result/${FAKE}/t1_m10000.txt
 
 # thread: 8
-# taskset -c 0-3 ./${FAKE} -t 8 -m 0 > result/${FAKE}/t8_m0.txt
-# taskset -c 0-3 ./${FAKE} -t 8 -m 25 > result/${FAKE}/t8_m25.txt
-# taskset -c 0-3 ./${FAKE} -t 8 -m 50 > result/${FAKE}/t8_m50.txt
-# taskset -c 0-3 ./${FAKE} -t 8 -m 100 > result/${FAKE}/t8_m100.txt
-# taskset -c 0-3 ./${FAKE} -t 8 -m 200 > result/${FAKE}/t8_m200.txt
-# taskset -c 0-3 ./${FAKE} -t 8 -m 500 > result/${FAKE}/t8_m500.txt
-# taskset -c 0-3 ./${FAKE} -t 8 -m 1000 > result/${FAKE}/t8_m1000.txt
+taskset -c 0-3 ./${FAKE} -t 8 -m 0 > result/${FAKE}/t8_m0.txt
+taskset -c 0-3 ./${FAKE} -t 8 -m 25 > result/${FAKE}/t8_m25.txt
+taskset -c 0-3 ./${FAKE} -t 8 -m 50 > result/${FAKE}/t8_m50.txt
+taskset -c 0-3 ./${FAKE} -t 8 -m 100 > result/${FAKE}/t8_m100.txt
+taskset -c 0-3 ./${FAKE} -t 8 -m 200 > result/${FAKE}/t8_m200.txt
+taskset -c 0-3 ./${FAKE} -t 8 -m 500 > result/${FAKE}/t8_m500.txt
+taskset -c 0-3 ./${FAKE} -t 8 -m 1000 > result/${FAKE}/t8_m1000.txt
 taskset -c 0-3 ./${FAKE} -t 8 -m 5000 > result/${FAKE}/t8_m5000.txt
 taskset -c 0-3 ./${FAKE} -t 8 -m 10000 > result/${FAKE}/t8_m10000.txt
 
